1d04645d4c7f751089bc14af5a3b5621765c2658
Rohith <rohithsai8899@gmail.com>
2025-06-26 14:40:14 +0530

    Added the caps,inductance functionality and done with the readme file

--- Circuit-Simulator---MATLAB/Circuit Simulator/GProducer.m
+++ Circuit-Simulator---MATLAB/Circuit Simulator/GProducer.m
@@ -1,47 +1,65 @@
-%This function will create a G matrix
+%This function will create a G matrix with R, C, L support
 function G = GProducer(inputCell)   
 
-%Finding conductance will help
-%I and V values will not be used
+syms s; % Laplace variable
 
-t = length(inputCell{4});
-Conductance = zeros(t,1);
+n = max(inputCell{3}); % G will be nxn matrix
+G = sym(zeros(n));     % Use symbolic zeros
 
-for i = 1:length(inputCell{4})
-Conductance(i) = 1/inputCell{4}(i);
-end
-
-n = max(inputCell{3});% G will be nxn matrix
-
-%Let start with nxn 0 matrix and add needed values to this matrix
-G = zeros(n);
-
-%Next loop makes diagonal part of G
+% Loop to fill diagonal terms
 for i = 1:length(inputCell{1})
-       %The condition verifies it is a resistor. Counts how much R
-       %in that value. If it is 'Rn', value will be 1, else it will be 0
-    if (sum(count(inputCell{1}(i),'R')) == 1)
-    %If resistance is connected between two nodes rather than ground(0 node)
-        if (inputCell{2}(i) ~= 0)
-              s = inputCell{2}(i);
-    G(s,s) = G(s,s) + Conductance(i);
-        r = inputCell{3}(i);
-    G(r,r) = G(r,r) + Conductance(i);
-        % The other case is when the resistance is connected ground(0 node)
-        else 
-       p = inputCell{3}(i);
-    G(p,p) = G(p,p) + Conductance(i);
-end
+    name = inputCell{1}{i};
+    value = inputCell{4}(i);
+
+    % Determine the admittance based on element type
+    if (sum(count(name, 'R')) == 1)
+        Y = 1 / value;
+    elseif (sum(count(name, 'C')) == 1)
+        Y = s * value;              % Capacitor: Y = sC
+    elseif (sum(count(name, 'L')) == 1)
+        Y = 1 / (s * value);        % Inductor: Y = 1/sL
+    else
+        continue;  % skip if not R, C, or L
+    end
+
+    % If element is between two nodes (not ground)
+    if inputCell{2}(i) ~= 0
+        s_node = inputCell{2}(i);
+        G(s_node, s_node) = G(s_node, s_node) + Y;
+        
+        r_node = inputCell{3}(i);
+        G(r_node, r_node) = G(r_node, r_node) + Y;
+    else
+        % If connected to ground
+        p = inputCell{3}(i);
+        G(p, p) = G(p, p) + Y;
     end
 end
-%Next loop makes off diagonal part of G
+
+% Loop to fill off-diagonal terms
 for i = 1:length(inputCell{1})
-    %The second condition verifies it is a resistor. Counts how much R
-       %in that value. If it is 'Rn', value will be 1, else it will be 0
-    if (inputCell{2}(i) ~= 0) && (sum(count(inputCell{1}(i),'R')) == 1)
+    name = inputCell{1}{i};
+    value = inputCell{4}(i);
+
+    % Determine the admittance again
+    if (sum(count(name, 'R')) == 1)
+        Y = 1 / value;
+    elseif (sum(count(name, 'C')) == 1)
+        Y = s * value;
+    elseif (sum(count(name, 'L')) == 1)
+        Y = 1 / (s * value);
+    else
+        continue;
+    end
+
+    % Only for non-ground connected components
+    if inputCell{2}(i) ~= 0
         k = inputCell{2}(i);
         m = inputCell{3}(i);
-        G(k,m) = G(k,m) - Conductance(i);
-        G(m,k) = G(m,k) - Conductance(i);
+
+        G(k, m) = G(k, m) - Y;
+        G(m, k) = G(m, k) - Y;
     end
+end
+
 end
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# Cricuit_Simulator
-We've built a circuit Simulator using Matlab and which solves circuits involved with passive components(resistors, capacitors, inductors)
+Circuit-Simulator---MATLAB
+This circuit simulator reads a text input file representing the circuit and determines the node voltages using the Modified Node Analysis algorithm.
+
+This circuit simulator works for any circuit which consists of only independent voltage sources, independent current sources, and passive elements such as Resistors, Capacitors, and Inductors.
+
+Text input file must include a net list of the circuit. Creating the netlist is visualized by an image. Check the "Netlist Example.PNG" file and also the "Netlist Example.txt" file in the Circuit Simulator directory.
+
+Modified Node Analysis (MNA) Algorithm:
+MNA applied to a circuit with only passive elements and independent current and voltage sources results in a matrix equation of the form:
+A ∙ x = z
+Let’s suppose we have the following:
+
+For a circuit with n nodes and m independent voltage sources:
+
+The A matrix:
+is of size (n+m) × (n+m), and consists only of known quantities.
+
+The upper-left nxn part (called G matrix):
+
+Contains only passive elements.
+
+Elements connected to ground appear only on the diagonal.
+
+Elements not connected to ground appear on both diagonal and off-diagonal positions.
+
+The rest of the A matrix (outside the nxn block) contains only 1, -1, and 0.
+
+The x vector:
+is an (n+m)×1 column vector holding the unknown quantities (node voltages and currents through the independent voltage sources).
+
+Top n entries represent the n node voltages.
+
+Bottom m entries represent the currents through the m independent voltage sources.
+
+The z vector:
+is an (n+m)×1 column vector containing known values.
+
+The top n elements correspond to contributions from independent current sources.
+
+The bottom m elements are values of the m independent voltage sources.
+
+Support for Capacitors and Inductors
+This simulator also supports capacitors (C) and inductors (L) using symbolic Laplace-domain analysis.
+
+Capacitors are modeled as:
+
+𝑌c = 𝑠⋅𝐶
+
+Inductors are modeled as:
+
+𝑌L = 1/𝑠⋅𝐿
+​ 
+Resistors are modeled as:
+
+𝑌𝑅=1/R
+ 
+When multiple passive elements (R, C, L) are connected between the same two nodes, their admittances are summed (i.e., treated as connected in parallel). For example, if R1, C1, and L1 are between nodes A and B, the total admittance added to the G matrix is:
+
+𝑌𝑡o𝑡a𝑙= 𝑠⋅𝐶 + 1/𝑠⋅𝐿 + 1/R
+
+ 
+The system is solved in the s-domain symbolically. After solving x = A \ z, the solution is transformed back into the time domain using MATLAB’s ilaplace() function:
+
+x_time = ilaplace(x, s, t);
+
+REFERENCES:
+1. 2024,  EE 204 Circuit Theory Course, IIT GUWAHATI.
+2. http://www.swarthmore.edu/NatSci/echeeve1/Ref/mna/MNA3.html
+3. http://www.swarthmore.edu/NatSci/echeeve1/Ref/mna/MNA6.html
